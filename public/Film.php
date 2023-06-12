b7d8bac5005c263ac0444ba3f54e87af428ca674
JACQUEMAIN Paco <paco.jacquemain@etudiant.univ-reims.fr>
2023-06-12 17:32:57 +0200

    Affichage rudimentaire de l'ensemble des film par leur titre

--- public/Film.php
+++ public/Film.php
@@ -1,3 +1,31 @@
 <?php
 declare(strict_types=1);
 
+use Database\MyPdo;
+use Html\AppWebPage;
+
+MyPDO::setConfiguration('mysql:host=mysql;dbname=jacq0223;charset=utf8', 'jacq0223', 'jacq0223');
+
+$webPage = new AppWebPage('Movies');
+
+
+
+$sql =
+    <<<'SQL'
+    SELECT title
+    FROM jacq0223.movie
+    SQL;
+
+$stmt = MyPdo::getInstance()->prepare($sql);
+
+$stmt->execute();
+$html = '';
+while (($ligne = $stmt->fetch()) !== false) {
+    $html.= "<p>";
+    $html.= $webPage->escapeString("{$ligne['title']}");
+    //echo $html;
+}
+$webPage->appendContent($html);
+
+echo $webPage->toHTML();
+
