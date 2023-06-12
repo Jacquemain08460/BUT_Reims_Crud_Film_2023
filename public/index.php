50d11898a7b526413e444416d743ff7b95834d23
FAY Damien <damien.fay@etudiant.univ-reims.fr>
2023-06-12 17:00:06 +0200

    Ajout de la classe Acteur et de ses méthodes

--- public/index.php
+++ public/index.php
@@ -3,3 +3,20 @@
 declare(strict_types=1);
 
 require_once '../vendor/autoload.php';
+
+use Database\MyPdo;
+use Entity\Actor;
+use Html\AppWebPage;
+
+if (!isset($_GET["artistId"]) || !ctype_digit($_GET["artistId"])) {
+    header("Location: index.php");
+    exit(404);
+}
+
+try {
+    $artist = Artist::findById((int)$_GET['artistId']);
+} catch (\Entity\Exception\EntityNotFoundException) {
+    header("Location: index.php");
+    http_response_code(404);
+    exit(404);
+}
