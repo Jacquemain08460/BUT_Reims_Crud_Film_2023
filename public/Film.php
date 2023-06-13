ab7dd000af54d24032795ba9cc9458697f14032e
JACQUEMAIN Paco <paco.jacquemain@etudiant.univ-reims.fr>
2023-06-13 10:53:48 +0200

    Avancement Film.php

--- public/Film.php
+++ public/Film.php
@@ -2,6 +2,7 @@
 declare(strict_types=1);
 
 use Database\MyPdo;
+use Entity\Image;
 use Html\AppWebPage;
 
 MyPDO::setConfiguration('mysql:host=mysql;dbname=jacq0223;charset=utf8', 'jacq0223', 'jacq0223');
@@ -25,20 +26,9 @@ while (($ligne = $stmt->fetch()) !== false) {
     //echo $html;
 
     $movieId = $ligne;
-    $sql =
-        <<<'SQL'
-        SELECT jpeg
-        FROM jacq0223.image
-        WHERE id= 
-        SQL;
-        $sql .= "{$ligne['posterId']}";
+    $image = Image::findById($ligne['posterId']);
 
-
-    $stmtPoster = MyPdo::getInstance()->prepare($sql);
-    $stmtPoster->execute();
-    while (($jpeg = $stmtPoster->fetch()) !== false){
-        $html.= $jpeg['jpeg'];
-    }
+    $html.= "<img src ={$image->show()} alt='HTML5 Icon' style='width:128px;height:128px'>";
 
 
 
