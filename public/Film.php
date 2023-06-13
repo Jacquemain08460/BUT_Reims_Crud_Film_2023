fc50b690afb9ce936156710e7da979bf4414c3b4
JACQUEMAIN Paco <paco.jacquemain@etudiant.univ-reims.fr>
2023-06-13 09:29:23 +0200

    Avancement affichage poster de film

--- public/Film.php
+++ public/Film.php
@@ -8,22 +8,42 @@ MyPDO::setConfiguration('mysql:host=mysql;dbname=jacq0223;charset=utf8', 'jacq02
 
 $webPage = new AppWebPage('Movies');
 
-
-
 $sql =
     <<<'SQL'
-    SELECT title
+    SELECT *
     FROM jacq0223.movie
     SQL;
 
 $stmt = MyPdo::getInstance()->prepare($sql);
 
+
 $stmt->execute();
 $html = '';
 while (($ligne = $stmt->fetch()) !== false) {
-    $html.= "<p>";
-    $html.= $webPage->escapeString("{$ligne['title']}");
+    $html.= "<div>";
+    $title= $webPage->escapeString("{$ligne['title']}"."</div>");
     //echo $html;
+
+    $movieId = $ligne;
+    $sql =
+        <<<'SQL'
+        SELECT jpeg
+        FROM jacq0223.image
+        WHERE id= 
+        SQL;
+        $sql .= "{$ligne['posterId']}";
+
+
+    $stmtPoster = MyPdo::getInstance()->prepare($sql);
+    $stmtPoster->execute();
+    while (($jpeg = $stmtPoster->fetch()) !== false){
+        $html.= $jpeg['jpeg'];
+    }
+
+
+
+    $html.= "<p>".$title."</p></div>";
+
 }
 $webPage->appendContent($html);
 
