1c24cc2e945d84ebd6c561f39e3b4a90f912a7ee
JACQUEMAIN Paco <paco.jacquemain@etudiant.univ-reims.fr>
2023-06-14 00:27:29 +0200

    Rajout du poster du film, du liens de la page du film, et de son titre

--- public/index.php
+++ public/index.php
@@ -10,31 +10,31 @@ use Entity\Image;
 use Html\AppWebPage;
 #MyPDO::setConfiguration('mysql:host=mysql;dbname=cutron01_music;charset=utf8', 'web', 'web');
 
-$All = new All\AllActors();
+#$All = new All\AllActors();
 
 #$All = Actor::getAll();
 
-$WebPage = new AppWebPage('Artistes');
+#WebPage = new AppWebPage('Artistes');
 
-$Actors = $All -> getAll();
+#$Actors = $All -> getAll();
 
 #var_dump($Actors);
 
-foreach ($Actors as $Actor) {
+#foreach ($Actors as $Actor) {
     #echo(gettype($Actor));
     #var_dump($Actor);
     #echo($Actor->getName());
-    $WebPage->appendContent("<a href='actor.php?actorId={$Actor->getId()}&artistName={$Actor->getName()}'>{$WebPage -> escapeString($Actor -> getName())}</a><hr>");
-}
+ #   $WebPage->appendContent("<a href='actor.php?actorId={$Actor->getId()}&artistName={$Actor->getName()}'>{$WebPage -> escapeString($Actor -> getName())}</a><hr>");
+#}
 
 $All = Movie::getAll();
 
 $WebPage = new AppWebPage('Movies');
 
 foreach ($All as $Movie) {
-    #echo(gettype($Movie));
-    #var_dump($Movie);
-    $WebPage->appendContent("<a href='actor.php?movieId={$Movie->getmovieId()}'>{Image::get}{$WebPage -> escapeString($Movie -> getTitle())}</a><hr>");
+    $WebPage->appendContent("<a href='FilmChosen.php?movieId={$Movie->getId()}'><di>");
+    $WebPage->appendContent("<img src='Image.php?imageId={$Movie->getPosterId()}'>");
+    $WebPage->appendContent("{$WebPage -> escapeString($Movie -> getTitle())}</a><hr></div>");
 }
 
 echo $WebPage->toHTML();
