c53fec3cc56e87382693df52a7b0e3f9f461872b
FAY Damien <damien.fay@etudiant.univ-reims.fr>
2023-06-13 14:55:47 +0200

    Transfert des fonctions d'Actor.php à Movie.php (modification d'instances)

--- src/Entity/Actor.php
+++ src/Entity/Actor.php
@@ -15,7 +15,7 @@ use PDO;
 
 class Actor
 {
-    private int|null $actorid;
+    private int $actorid;
     private string $name;
     private string $birthday;
     private string|null $deathday;
@@ -187,8 +187,6 @@ class Actor
     SQL
         );
         $stmt->execute([":ID" => $this->actorid]);
-        $this->null;
-        return $this;
     }
 
     public function update()
@@ -267,3 +265,6 @@ class Actor
         return $res[0];
     }
 }
+
+
+#posterId, originalLanguage, originalTitle, overview, releaseDate, runtime, tagline, title, movieId
